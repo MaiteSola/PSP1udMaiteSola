c12ded0f34331ef18a9c78a3562429887185716a
Maite <maitesolam@gmail.com>
2025-09-15 11:00:25 +0200

    Esqueleto 1ud PSP

---
+++ src/main/java/org/cuatrovientos/dam/psp/Introduccion/ud/App.java
@@ -0,0 +1,13 @@
+package org.cuatrovientos.dam.psp.Introduccion.ud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
